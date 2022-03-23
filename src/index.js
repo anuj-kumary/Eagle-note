e11dc9d4d7c43ae2bd8fdf7a63d47cb0050551d2
anuj-kumary <anujf0510@gmail.com>
2022-03-23 20:50:04 +0530

    folder structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { makeServer } from './server';
 
 // Call make Server
 makeServer();
@@ -11,5 +11,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
